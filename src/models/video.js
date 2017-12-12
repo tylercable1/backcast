65b46d3156c70af5ac274ff2c8095caf0c78fc1e
tylercable1 <tylercable7@gmail.com>
2017-12-12 12:51:40 -0800

    Fix this.set in video

--- src/models/video.js
+++ src/models/video.js
@@ -2,7 +2,9 @@ var Video = Backbone.Model.extend({
 
   initialize: function(video) {
     // override youtube's complex id field
-    this.set({id: video.id.videoId, title: video.snippet.title, description: video.snippet.description, thumbnail: video.snippet.thumbnails.default.url});
+    // debugger;
+    this.set('id', video.id.videoId);
+// , title: video.snippet.title, description: video.snippet.description, thumbnail: video.snippet.thumbnails.default.url});
   },
 
   select: function() {

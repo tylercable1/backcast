5636cf75865bc1356333d98c553b458f3aa18b5d
tylercable1 <tylercable7@gmail.com>
2017-12-12 09:57:32 -0800

    Add thumbnails to videlistentries

--- src/models/video.js
+++ src/models/video.js
@@ -2,7 +2,7 @@ var Video = Backbone.Model.extend({
 
   initialize: function(video) {
     // override youtube's complex id field
-    this.set({id: video.id.videoId, title: video.snippet.title, description: video.snippet.description});
+    this.set({id: video.id.videoId, title: video.snippet.title, description: video.snippet.description, thumbnail: video.snippet.thumbnails.default.url});
   },
 
   select: function() {

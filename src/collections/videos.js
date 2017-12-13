9304546768168f44bb3303abe2547ce936819497
tylercable1 <tylercable7@gmail.com>
2017-12-12 16:23:30 -0800

    search working on initialization

--- src/collections/videos.js
+++ src/collections/videos.js
@@ -1,5 +1,14 @@
 var Videos = Backbone.Collection.extend({
 // change
-  model: Video
+  model: Video,
 
+  url: 'https://www.googleapis.com/youtube/v3/search',
+  
+  search: function(query) {
+    this.fetch({data: {q: query, part: 'snippet', maxResults: 5, key: window.YOUTUBE_API_KEY, type: 'video', videoEmbeddable: 'true'}});
+  },
+
+  parse: function(data) {
+    return data.items;
+  }
 });
--- src/views/app.js
+++ src/views/app.js
@@ -3,7 +3,9 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos(window.exampleVideoData);
+    // this.videos = new Videos(window.exampleVideoData);
+    this.videos = new Videos();
+    this.videos.search('dogs');
     this.render();
   },
 
